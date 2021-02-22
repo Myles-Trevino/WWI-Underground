10e40cbf65bb75d57493d236f429123760926237
jacksonjreed <50586789+jacksonjreed@users.noreply.github.com>
2021-02-22 16:10:01 -0500

    added caption text style

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,6 +13,7 @@ import Constants from '../common/constants';
 import * as Helpers from '../common/helpers';
 import Styles from './about.module.scss';
 import Navbar from '../components/common/navbar/navbar';
+import { warnAboutProxyRequirement } from 'mobx/dist/internal';
 
 export default function Index(): JSX.Element {
 
@@ -51,7 +52,7 @@ export default function Index(): JSX.Element {
 			<div className={Styles.section}>
 				<h2>Photographing the caves</h2>
 				<img src="images/about/_DSC2466.jpg" width="100%"/>
-				<p>[caption]</p>
+				<p className="caption">The team, including Gilles Chauwin, Kelly Goff, Mark LeBlanc, and Keith Heyward, set up equipment in the cave.</p>
 				<p>In August of 2019, a team from Wheaton College in Norton, Massachusetts undertook a two-day expedition (totalling ~20 hours of work) into the caves at Braye-en-Laonnois, France to capture the panorama images that comprise the tour on this site along with 3D Lidar laser scans.</p>
 				<p>The Picardy region of France is peppered with abandoned quarries, many dating to the Middle Ages and mined for stone used in the construction of dwellings from cottages to cathedrals. Some of these caverns are miles in length. During World War I, the caves were occupied by U.S. National Guard soldiers from New England—from the American Expeditionary Force (AEF) 26th “Yankee” Division.</p>
 				<p>One of the three quarries is no longer accessible. The two remaining quarries are filled with carvings and writings made by American soldiers. Artifacts in both quarries have suffered from vandalism and theft, as well as natural environmental effects that have led to cave-ins and water damage to the sites. These sites face an uncertain and, potentially, imperiled future. Without efforts to document the carvings and writings of these Americans, their messages—sometimes the last
