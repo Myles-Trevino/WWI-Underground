48ab800fbdf5efec7f622e24bbfd2faf21bbceb9
jacksonjreed <50586789+jacksonjreed@users.noreply.github.com>
2021-02-22 16:43:05 -0500

    alt text, added content

--- pages/about.tsx
+++ pages/about.tsx
@@ -51,8 +51,8 @@ export default function Index(): JSX.Element {
 
 			<div className={Styles.section}>
 				<h2>Photographing the caves</h2>
-				<img src="images/about/_DSC2466.jpg" width="100%"/>
-				<p className="caption">The team, including Gilles Chauwin, Kelly Goff, Mark LeBlanc, and Keith Heyward, set up equipment in the cave.</p>
+				<img src="images/about/_DSC2466.jpg" width="100%" alt="The team, including Gilles Chauwin, Kelly Goff, Keith Heyward, and Mark LeBlanc, set up equipment in the cave."/>
+				<p className="caption">The team, including Gilles Chauwin, Kelly Goff, Keith Heyward, and Mark LeBlanc, set up equipment in the cave.</p>
 				<p>In August of 2019, a team from Wheaton College in Norton, Massachusetts undertook a two-day expedition (totalling ~20 hours of work) into the caves at Braye-en-Laonnois, France to capture the panorama images that comprise the tour on this site along with 3D Lidar laser scans.</p>
 				<p>The Picardy region of France is peppered with abandoned quarries, many dating to the Middle Ages and mined for stone used in the construction of dwellings from cottages to cathedrals. Some of these caverns are miles in length. During World War I, the caves were occupied by U.S. National Guard soldiers from New England—from the American Expeditionary Force (AEF) 26th “Yankee” Division.</p>
 				<p>One of the three quarries is no longer accessible. The two remaining quarries are filled with carvings and writings made by American soldiers. Artifacts in both quarries have suffered from vandalism and theft, as well as natural environmental effects that have led to cave-ins and water damage to the sites. These sites face an uncertain and, potentially, imperiled future. Without efforts to document the carvings and writings of these Americans, their messages—sometimes the last
@@ -61,13 +61,13 @@ message from a soldier—may be lost forever. Securing a 3D footprint is one imm
 
 			<div className={Styles.section}>
 				<h2>The 26th Yankee Division</h2>
-
-				<p>[todo]</p>
+				<img src="images/about/from-maine-to-france-cover.jpg" width="35%" alt="Book cover with a photo of a seated soldier and the text: From Maine to France and Somehow Back Again. World War I Experiences of John M. Longley and the 26th Yankee Division. Mark D. LeBlanc and John M. LeBlanc."/>
+				<p>This project follows over a decade of research by Mark LeBlanc (documented in his book <a href="https://www.lulu.com/en/us/shop/john-m-leblanc-and-mark-d-leblanc/from-maine-to-france-and-somehow-back-again-world-war-i-experiences-of-john-m-longley-and-the-26th-yankee-division/paperback/product-1p7jqek9.html">From Maine to France</a>) and Christine Pittsley (for <a href="https://ctstatelibrary.org/remembering-world-war-one/">Remembering World War One</a> at the CT State Library), investigating the experiences of New England soldiers in WWI.</p>
 			</div>
 
 			<div className={Styles.section}>
 				<h2>Press</h2>
-				<img src="images/about/WW1_logo_v3_white.png" width="50%"/>
+				<img src="images/about/WW1_logo_v3_white.png" width="50%" alt="Logo of the the United States WWI Centennial Commission."/>
 				<p><a href="https://www.worldwar1centennial.org/index.php/communicate/press-media/wwi-centennial-news/6555-project-demonstrates-3d-collection-of-cave-messages-in-france-left-by-american-soldiers-in-wwi.html">Project demonstrates 3D collection of cave messages in France left by American soldiers in WWI</a> from the United States WWI Centennial Commission</p>
 				<p><a href="https://wheatoncollege.blog/academics/computer-science/100-years-pass-like-a-dream/">100 years pass like a dream</a> from Wheaton College</p>
 			</div>
